aef67be8980854d84e2fe98f06a318127afe07e6
Carla Zacarias <caz35@txstate.edu>
2020-08-06 15:34:35 -0500

    added more

--- test.py
+++ test.py
@@ -14,17 +14,27 @@ class TestFunctions(unittest.TestCase):
 	def test_populateUsers(self, mock_populateUsers):
 		main._populateUsers()
 		self.assertIsNotNone(allUsers)
+		print("testing _populateUsers")
 
 	@patch('main._populateLogsTab')
 	def test_populateLogsTab(self, mock_populateLogsTab):
 		main._populateLogsTab()
 		self.assertIsNotNone(allLogs)
+		print("testing _populateLogsTab")
 
 	@patch('requests.post')
 	def test_postToLog(self, mock_post):
 		data = {"name": "carla", "device": "speaker", "action": "turn-on"}
 		req = requests.post(url + "/logs", data = json.dumps(data))
 		mock_post.assert_called_with(url + "/logs", data = json.dumps(data))
+		print("testing post method")
+
+	def test_get(self):
+		r = requests.get(url + "/allLogs")
+		if r.status_code == 200:
+			print("success")
+		elif r.status_code == 404:
+			print("failure")
 
 
 if __name__ == '__main__': 
